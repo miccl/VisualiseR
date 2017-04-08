06d6f70f078acdad14f2ad2603ff2206f1b0c290
miccl <hertha_mcc@hotmail.de>
2017-04-08 17:57:27 +0200

    Initial commit

---
+++ Assets/Scripts/Convert.cs
@@ -0,0 +1,20 @@
+﻿
+
+public class Convert {
+
+
+    public Convert(string filePath) {
+        this.filePath = filePath;
+    }
+
+    private string filePath;
+
+    public convert() {
+
+    }
+
+
+    public convertPDFToPNG() {
+
+    }
+}
