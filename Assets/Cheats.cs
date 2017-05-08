38d44397813778a4cbe0065078fe7dddc9e3bf7b
miccl <hertha_mcc@hotmail.de>
2017-05-08 16:16:44 +0200

    Add Drag and Drop

---
+++ Assets/Cheats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Cvheats : MonoBehaviour
+{
+    private bool vrModeEnabled = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("VrMode"))
+        {
+            vrModeEnabled = !vrModeEnabled;
+            GvrViewer.Instance.VRModeEnabled = vrModeEnabled;
+        }
+    }
+}
