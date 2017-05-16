53f1682c41b594ba05ea89a9d3bda3d450a7977d
miccl <hertha_mcc@hotmail.de>
2017-05-16 13:37:19 +0200

    Added methods to computate spawn positions

---
+++ Assets/VisualiseR/Code/Editor/VisualiseR/Common/Util/MathUtilTest.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using VisualiseR.Util;
+
+namespace VisualiseR.Common
+{
+    public class MathUtilTest
+    {
+        [TestFixture]
+        public class ComputeSpawnPositionMethod
+        {
+            [Test]
+            public void SimpleSpawnPositions()
+            {
+                //Start Position
+                Vector3 actualStartPosition = MathUtil.ComputeSpawnPosition(5, 0, 2);
+                Vector3 expectedStartPosition = new Vector3(5, 2, 0);
+                Assert.IsTrue(actualStartPosition == expectedStartPosition);
+
+                //Middle position
+                Vector3 actualMiddlePosition = MathUtil.ComputeSpawnPosition(5, 45, 2);
+                Vector3 expectedMiddlePosition = new Vector3(3.5f, 2, 3.5f);
+                Assert.IsTrue(actualMiddlePosition == expectedMiddlePosition);
+                //End position
+                Vector3 actualEndPosition = MathUtil.ComputeSpawnPosition(5, 90, 2);
+                Vector3 expectedEndPosition = new Vector3(0, 2, 5);
+                Assert.IsTrue(actualEndPosition == expectedEndPosition);
+            }
+
+            [Test]
+            public void EndPosition()
+            {
+                //Start Position
+            }
+        }
+
+        [TestFixture]
+        public class ComputeSpawnPositionMethodFromFront
+        {
+            [Test]
+            public void SimpleSpawnPositions()
+            {
+                //Start Position
+                Vector3? actualStartPosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 0, 2);
+                Vector3 expectedStartPosition = new Vector3(0, 2, 5);
+//                StringAssert.AreEqualIgnoringCase(actualStartPosition.ToString(), "hallo");
+                AssertVectorsAreEqual(expectedStartPosition, (Vector3) actualStartPosition);
+//                //Middle position
+                Vector3? actualMiddlePosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 45, 2);
+                Vector3 expectedMiddlePosition = new Vector3(-3.5f, 2f, 3.5f);
+                AssertVectorsAreEqual(expectedMiddlePosition, (Vector3) actualMiddlePosition);
+//                StringAssert.AreEqualIgnoringCase(actualMiddlePosition.ToString(),"E" + expectedMiddlePosition);
+//                Assert.IsTrue(actualMiddlePosition == expectedMiddlePosition);
+
+                //End position
+                Vector3? actualEndPosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 90, 2);
+                Vector3 expectedEndPosition = new Vector3(-5, 2, 0);
+                AssertVectorsAreEqual(expectedEndPosition, (Vector3) actualEndPosition);
+            }
+
+            [Test]
+            public void SimpleSpawnPositions360Degree()
+            {
+                //Start Position
+                Vector3? actualStartPosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 0, 2);
+                Vector3 expectedStartPosition = new Vector3(0, 2, 5);
+//                StringAssert.AreEqualIgnoringCase(actualStartPosition.ToString(), "hallo");
+                AssertVectorsAreEqual(expectedStartPosition, (Vector3) actualStartPosition);
+//                //Middle position
+                Vector3? actualMiddlePosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 45, 2);
+                Vector3 expectedMiddlePosition = new Vector3(-3.5f, 2f, 3.5f);
+                AssertVectorsAreEqual(expectedMiddlePosition, (Vector3) actualMiddlePosition);
+//                StringAssert.AreEqualIgnoringCase(actualMiddlePosition.ToString(),"E" + expectedMiddlePosition);
+//                Assert.IsTrue(actualMiddlePosition == expectedMiddlePosition);
+
+                //End position
+                Vector3? actualEndPosition = MathUtil.ComputeSpawnPositionFromStartPosition(5, 90, 2);
+                Vector3 expectedEndPosition = new Vector3(-5, 2, 0);
+                AssertVectorsAreEqual(expectedEndPosition, (Vector3) actualEndPosition);
+            }
+        }
+
+
+        [TestFixture]
+        public class ComputeSpawnPositionsWithAngleMethod
+        {
+            [Test]
+            public void Angle30()
+            {
+                //given
+                float spawnDistance = 5;
+                float angleBetweenElements = 45;
+                int radius = 90;
+                float posY = 2;
+
+                //when
+                List<Vector3> positions =
+                    MathUtil.ComputeSpawnPositionsWithAngle(spawnDistance, angleBetweenElements, radius, posY);
+
+                //then
+                Assert.AreEqual(positions.Count, 3);
+                AssertVectorsAreEqual(positions[0], new Vector3(0, posY, spawnDistance));
+                AssertVectorsAreEqual(positions[1], new Vector3(-3.5f, posY, 3.5f));
+                AssertVectorsAreEqual(positions[positions.Count - 1], new Vector3(-spawnDistance, posY, 0));
+            }
+        }
+
+        [TestFixture]
+        public class ComputeSpawnPositionsWithElements
+        {
+            [Test]
+            public void Simple()
+            {
+                //given
+                float spawnDistance = 5;
+                int numberOfElements = 3;
+                int radius = 90;
+                float posY = 2;
+
+                //when
+                List<Vector3> positions =
+                    MathUtil.ComputeSpawnPositionsWithElements(spawnDistance, numberOfElements, radius, posY);
+
+                //then
+                Assert.AreEqual(positions.Count, 3);
+                AssertVectorsAreEqual(positions[0], new Vector3(0, posY, spawnDistance));
+                AssertVectorsAreEqual(positions[1], new Vector3(-3.5f, posY, 3.5f));
+                AssertVectorsAreEqual(positions[positions.Count - 1], new Vector3(-spawnDistance, posY, 0));
+            }
+        }
+
+
+        internal static void AssertVectorsAreEqual(Vector3 expectedVector, Vector3 actualVector)
+        {
+            float tolerance = 0.1f;
+            Assert.AreEqual(expectedVector.x, actualVector.x, tolerance);
+            Assert.AreEqual(expectedVector.y, actualVector.y, tolerance);
+            Assert.AreEqual(expectedVector.z, actualVector.z, tolerance);
+        }
+    }
+}
---
+++ Assets/VisualiseR/Code/Scripts/VisualiseR/Util/MathUtil.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace VisualiseR.Util
+{
+    public static class MathUtil
+    {
+        public static List<Vector3> ComputeSpawnPositionsWithElements(float spawnDistance, int numberOfElements,
+            int radius, float posY)
+        {
+            List<Vector3> positions = new List<Vector3>();
+
+            float angleBetweenElements = radius / (float) (numberOfElements - 1);
+            float currentAngle = 0;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Vector3? currPos = ComputeSpawnPositionFromStartPosition(spawnDistance, currentAngle, posY);
+                if (currPos == null)
+                {
+                    break;
+                }
+                positions.Add((Vector3) currPos);
+                currentAngle += angleBetweenElements;
+            }
+            return positions;
+        }
+
+        public static List<Vector3> ComputeSpawnPositionsWithAngle(float spawnDistance, float angleBetweenElements,
+            int radius, float posY)
+        {
+            List<Vector3> positions = new List<Vector3>();
+            float currAngle = 0;
+            while (currAngle <= radius)
+            {
+                Vector3? currPos = ComputeSpawnPositionFromStartPosition(spawnDistance, currAngle, posY);
+                if (currPos == null)
+                {
+                    break;
+                }
+                positions.Add((Vector3) currPos);
+                currAngle += angleBetweenElements;
+            }
+
+            return positions;
+        }
+
+        [CanBeNull]
+        public static Vector3? ComputeSpawnPositionFromStartPosition(float spawnDistance, float spawnAngle, float posY)
+        {
+            if (spawnAngle > 360)
+            {
+                return null;
+            }
+
+            float startAngle = 90;
+            float spawnAngleFromStartPosition = spawnAngle + startAngle;
+
+            return ComputeSpawnPosition(spawnDistance, spawnAngleFromStartPosition, posY);
+        }
+
+        /// <summary>
+        /// Computes position with give parameters.
+        /// </summary>
+        /// <param name="spawnDistance">The distance between zero position</param>
+        /// <param name="spawnAngle">The angle degree.</param>
+        /// <param name="posY">The position in y angle.</param>
+        /// <returns></returns>
+        public static Vector3 ComputeSpawnPosition(float spawnDistance, float spawnAngle, float posY)
+        {
+            // convert from degree to radiance
+
+            float alphaRad = spawnAngle * Mathf.Deg2Rad;
+
+            // compute x and z position based on the random value, y pos is a random value between given yPosMin and xPosMax
+            float xPos = Mathf.Cos(alphaRad) * spawnDistance;
+            float yPos = posY;
+            float zPos = Mathf.Sin(alphaRad) * spawnDistance;
+
+            return new Vector3(xPos, yPos, zPos);
+        }
+
+    }
+}
