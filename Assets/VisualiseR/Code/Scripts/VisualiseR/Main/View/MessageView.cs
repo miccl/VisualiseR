138f44af7cda7a0af89d54ce0917dee092081d96
miccl <hertha_mcc@hotmail.de>
2017-06-02 15:40:25 +0200

    Refactoring main scene

--- Assets/VisualiseR/Code/Scripts/VisualiseR/Main/View/MessageView.cs
+++ Assets/VisualiseR/Code/Scripts/VisualiseR/Main/View/MessageView.cs
@@ -1,29 +1,29 @@
 ﻿using strange.extensions.mediation.impl;
 using UnityEngine;
 using UnityEngine.UI;
-using VisualiseR.Util;
 
 namespace VisualiseR.Main
 {
     public class MessageView : View
     {
+        private Transform _messagePanel;
+        private Transform _centerPanel;
         internal Text _infoText;
+        private Transform _title;
         internal Text _titleLabel;
-        private GameObject _centerPanel;
-        private Image _centerPanelImage;
-        private GameObject _menuCanvas;
         private Image _titleImage;
 
         protected override void Awake()
         {
             base.Awake();
-            _infoText = UnityUtil.FindGameObjectInChild("InfoText").GetComponent<Text>();
-            _titleLabel = transform.FindChild("Title").GetComponentInChildren<Text>();
-            _titleImage = transform.FindChild("Title").GetComponent<Image>();
-            _centerPanel = transform.FindChild("CenterPanel").gameObject;
+            _messagePanel = transform.Find("MessagePanel");
+            
+            _centerPanel = _messagePanel.FindChild("CenterPanel");
+            _infoText = _centerPanel.Find("InfoText").GetComponent<Text>();
 
-            _menuCanvas = UnityUtil.FindGameObject("MenuCanvas");
-
+            _title = _messagePanel.FindChild("Title");
+            _titleLabel = _title.GetComponentInChildren<Text>();
+            _titleImage = _title.GetComponent<Image>();
         }
 
         public void Init(MessageType type, string title, string message)
@@ -60,7 +60,7 @@ namespace VisualiseR.Main
 
         public void OnOkButtonClick()
         {
-            Destroy(gameObject.transform.parent.gameObject);
+            Destroy(gameObject);
         }
     }
 }
